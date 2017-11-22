ee464f3062d887d3b43cd683894f5c8a5fdeb19f
Chaahat <u6398806@anu.edu.au>
2017-11-22 14:45:59 +1100

    need to start Chapter 9

--- app/src/main/java/com/bignerdranch/android/crimeintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/crimeintent/CrimeActivity.java
@@ -1,9 +1,11 @@
 package com.bignerdranch.android.crimeintent;
 
+import android.annotation.SuppressLint;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+@SuppressLint("Registered")
 public class CrimeActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
